cfe675ac395cc0563f18f77ec847b41aa163e467
Kingkor Roy Tirtho <krtirtho@gmail.com>
2022-09-16 14:49:31 +0600

    latest fl-query infinite query implemented

---
+++ lib/hooks/usePaginatedInfiniteQuery.dart
@@ -0,0 +1,55 @@
+import 'package:fl_query/fl_query.dart';
+import 'package:fl_query_hooks/fl_query_hooks.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hookified_infinite_scroll_pagination/hookified_infinite_scroll_pagination.dart';
+
+class PagedQuery<T extends Object, Outside, PageParam extends Object,
+    ItemType> {
+  PagingController<PageParam, ItemType> pagingController;
+  InfiniteQuery<T, Outside, PageParam> query;
+  PagedQuery(this.pagingController, this.query);
+}
+
+PagedQuery<T, Outside, PageParam, ItemType> usePaginatedInfiniteQuery<
+    T extends Object, Outside, PageParam extends Object, ItemType>(
+  InfiniteQueryJob<T, Outside, PageParam> job, {
+  required Outside externalData,
+  required PageParam firstPageKey,
+  void Function(
+    T,
+    PagingController<PageParam, ItemType> pagingController,
+    PageParam pageKey,
+  )?
+      onData,
+  void Function(Object)? onError,
+}) {
+  final pagingController = usePagingController<PageParam, ItemType>(
+    firstPageKey: firstPageKey,
+  );
+  final mounted = useIsMounted();
+
+  final query = useInfiniteQuery<T, Outside, PageParam>(
+    job: job,
+    externalData: externalData,
+    onData: (page, pageParam, pages) {
+      if (!mounted()) return;
+      onData?.call(page, pagingController, pageParam);
+    },
+    onError: (error, pageParam, pages) {
+      if (!mounted()) return;
+      pagingController.error = error;
+      onError?.call(error);
+    },
+  );
+
+  useEffect(() {
+    listener(PageParam pageKey) {
+      query.fetchNextPage((_, __) => pageKey);
+    }
+
+    pagingController.addPageRequestListener(listener);
+    return () => pagingController.removePageRequestListener(listener);
+  }, [pagingController, query.fetchNextPage]);
+
+  return PagedQuery(pagingController, query);
+}
